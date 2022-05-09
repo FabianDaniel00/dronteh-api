4dbd55a0901867872d547e36cbec377ffa164b5c
FabianDaniel00 <danielfabian090@gmail.com>
2022-05-09 20:07:57 +0200

    LoginAuthenticator fix

--- apps/dronteh-api/src/Security/LoginAuthenticator.php
+++ apps/dronteh-api/src/Security/LoginAuthenticator.php
@@ -31,13 +31,13 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '');
+        $email = $request->request->get('login_form', ['email' => ''])['email'];
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($request->request->get('password', '')),
+            new PasswordCredentials($request->request->get('login_form', ['plainPassword' => ''])['plainPassword']),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
                 new RememberMeBadge(),
